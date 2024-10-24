98d38538ef5fa3b23eb66f43be12d9ee1f73394b
Gregory Chebatyuk <gchebatyu@gmail.com>
2024-10-24 16:38:11 +0300

    add final version

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
-// Декларативный
-import { createElement } from 'react';
-// import logo from './logo.svg';
+import { createElement, useState } from 'react';
+
 import './app.module.css';
 
-// Императивный
 export const App = () => {
-	// const year = new Date().getFullYear();
-	// Декларативный
+	const [value, setValue] = useState('');
+	const [list, setList] = useState([]);
+	const [error, setError] = useState('');
+
+	const isValueValid = value.length >= 3;
+
+	const onInputButtonClick = () => {
+		const promptValue = prompt('Введите значение:');
+		if (promptValue.length < 3) {
+			setError('Введенное значение должно содержать минимум 3 символа');
+		} else {
+			setValue(promptValue);
+			setError('');
+		}
+	};
+
+	const onAddButtonClick = () => {
+		if (isValueValid) {
+			const updatedList = [...list, { id: Date.now(), value }];
+			setList(updatedList);
+			setValue('');
+			setError('');
+		}
+	};
+
 	return createElement(
 		'div',
 		{ className: 'app' },
@@ -15,22 +36,31 @@ export const App = () => {
 			'p',
 			{ className: 'no-margin-text' },
 			'Текущее значение',
-			createElement('code', null, 'value'),
-			': "',
-			createElement('output', { className: 'current-value' }, ''),
+			createElement('code', null, ' value:'),
+			'',
+			createElement('output', { className: 'current-value' }, value),
 		),
-		createElement(
-			'div',
-			{ className: 'error' },
-			'Введенное значение должно содержать минимум 3 символа',
-		),
+		error &&
+			createElement(
+				'div',
+				{ className: 'error' },
+				'Введенное значение должно содержать минимум 3 символа',
+			),
 		createElement(
 			'div',
 			{ className: 'buttons-container' },
-			createElement('button', { className: 'button' }, 'Ввести новое'),
 			createElement(
 				'button',
-				{ className: 'button', disabled: true },
+				{ className: 'button', onClick: onInputButtonClick },
+				'Ввести новое',
+			),
+			createElement(
+				'button',
+				{
+					className: 'button',
+					disabled: !isValueValid,
+					onClick: onAddButtonClick,
+				},
 				'Добавить в список',
 			),
 		),
@@ -38,49 +68,25 @@ export const App = () => {
 			'div',
 			{ className: 'list-container' },
 			createElement('h2', { className: 'list-heading' }, 'Список:'),
-			createElement(
-				'p',
-				{ className: 'no-margin-text' },
-				'Нет добавленных элементов',
-			),
-			createElement(
-				'ul',
-				{ className: 'list' },
-				createElement('li', { className: 'list-item' }, 'Первый элемент'),
-			),
+			// Если список не пуст, отображаем его элементы, иначе сообщение о пустом списке
+			list.length > 0
+				? createElement(
+						'ul',
+						{ className: 'list' },
+						list.map(
+							(item) =>
+								createElement(
+									'li',
+									{ key: item.id, className: 'list-item' },
+									item.value,
+								), // Использование состояния list
+						),
+					)
+				: createElement(
+						'p',
+						{ className: 'no-margin-text' },
+						'Нет добавленных элементов',
+					),
 		),
 	);
 };
-
-// export const App = () => {
-// 	const year = new Date().getFullYear();
-// 	// Декларативный
-// 	return createElement(
-// 		'div',
-// 		{ className: 'App' },
-// 		createElement(
-// 			'header',
-// 			{ className: 'App-header' },
-// 			createElement('img', { src: logo, className: 'App-logo', alt: 'logo' }),
-// 			createElement(
-// 				'p',
-// 				null,
-// 				`Edit`,
-// 				createElement('code', null, `src / App.js`),
-// 				`and save to reload.`,
-// 			),
-// 			createElement(
-// 				'a',
-// 				{
-// 					className: 'App-link',
-// 					href: 'https://reactjs.org',
-// 					target: '_blank',
-// 					rel: 'noopener noreferrer',
-// 				},
-// 				`Learn React`,
-// 			),
-// 			createElement('div', null, year),
-// 		),
-// 	);
-
-// };

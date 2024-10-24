6f8cf1de41e2cce145b56450e61009f9bdc18cbc
Gregory Chebatyuk <gchebatyu@gmail.com>
2024-10-24 15:32:26 +0300

    add css&jsx

--- src/App.js
+++ src/App.js
@@ -1,57 +1,86 @@
 // Декларативный
 import { createElement } from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import './app.module.css';
 
 // Императивный
 export const App = () => {
-	const year = new Date().getFullYear();
+	// const year = new Date().getFullYear();
 	// Декларативный
 	return createElement(
 		'div',
-		{ className: 'App' },
+		{ className: 'app' },
+		createElement('h1', { className: 'page-heading' }, 'Ввод значения'),
 		createElement(
-			'header',
-			{ className: 'App-header' },
-			createElement('img', { src: logo, className: 'App-logo', alt: 'logo' }),
+			'p',
+			{ className: 'no-margin-text' },
+			'Текущее значение',
+			createElement('code', null, 'value'),
+			': "',
+			createElement('output', { className: 'current-value' }, ''),
+		),
+		createElement(
+			'div',
+			{ className: 'error' },
+			'Введенное значение должно содержать минимум 3 символа',
+		),
+		createElement(
+			'div',
+			{ className: 'buttons-container' },
+			createElement('button', { className: 'button' }, 'Ввести новое'),
+			createElement(
+				'button',
+				{ className: 'button', disabled: true },
+				'Добавить в список',
+			),
+		),
+		createElement(
+			'div',
+			{ className: 'list-container' },
+			createElement('h2', { className: 'list-heading' }, 'Список:'),
 			createElement(
 				'p',
-				null,
-				`Edit`,
-				createElement('code', null, `src / App.js`),
-				`and save to reload.`,
+				{ className: 'no-margin-text' },
+				'Нет добавленных элементов',
 			),
 			createElement(
-				'a',
-				{
-					className: 'App-link',
-					href: 'https://reactjs.org',
-					target: '_blank',
-					rel: 'noopener noreferrer',
-				},
-				`Learn React`,
+				'ul',
+				{ className: 'list' },
+				createElement('li', { className: 'list-item' }, 'Первый элемент'),
 			),
-			createElement('div', null, year),
 		),
 	);
-	// return (
-	// Декларативный
-	// 	<div className="App">
-	// 		<header className="App-header">
-	// 			<img src={logo} className="App-logo" alt="logo" />
-	// 			<p>
-	// 				Edit <code>src/App.js</code> and save to reload. 3
-	// 			</p>
-	// 			<a
-	// 				className="App-link"
-	// 				href="https://reactjs.org"
-	// 				target="_blank"
-	// 				rel="noopener noreferrer"
-	// 			>
-	// 				Learn React
-	// 			</a>
-	// 			<div>{year}</div>
-	// 		</header>
-	// 	</div>
-	// );
 };
+
+// export const App = () => {
+// 	const year = new Date().getFullYear();
+// 	// Декларативный
+// 	return createElement(
+// 		'div',
+// 		{ className: 'App' },
+// 		createElement(
+// 			'header',
+// 			{ className: 'App-header' },
+// 			createElement('img', { src: logo, className: 'App-logo', alt: 'logo' }),
+// 			createElement(
+// 				'p',
+// 				null,
+// 				`Edit`,
+// 				createElement('code', null, `src / App.js`),
+// 				`and save to reload.`,
+// 			),
+// 			createElement(
+// 				'a',
+// 				{
+// 					className: 'App-link',
+// 					href: 'https://reactjs.org',
+// 					target: '_blank',
+// 					rel: 'noopener noreferrer',
+// 				},
+// 				`Learn React`,
+// 			),
+// 			createElement('div', null, year),
+// 		),
+// 	);
+
+// };
